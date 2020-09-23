46d1ac2405e87e877e268e3e0aed2dfdb4a4a4bd
Corrado Mio <corrado.mio@unimi.it>
2020-09-23 22:14:30 +0400

    Update implementation

--- check_tracks/src/other.cpp
+++ check_tracks/src/other.cpp
@@ -2,43 +2,53 @@
 // Created by Corrado Mio on 19/09/2020.
 //
 
-//
-//void crete_grids() {
-//
-//    create_grid(5, 0, grid_fname(5, 0));
-//
-//    std::vector<int> sides{5,10,20,50,100};
-//    std::vector<int> intervals{1,5,10,15,30,60};
-//
-//    for(int side : sides) {
-//        tbb::parallel_for_each(intervals.begin(), intervals.end(), [&](int interval) {
-//            std::string fname = grid_fname(side, interval);
-//            std::cout << fname << std::endl;
-//
-//            create_grid(side, interval, grid_fname(side, interval));
-//        });
-//    }
-//
-//}
+#include "dworld.h"
+#include <tbb/parallel_for_each.h>
 
-//void load_grids() {
-//
-//    {
-//        DiscreteWorld dworld;
-//        load_grid(dworld, grid_fname(5, 0));
-//    }
-//
-//    std::vector<int> sides{5,10,20,50,100};
-//    std::vector<int> intervals{1,5,10,15,30,60};
-//
-//    for(int side : sides) {
-//        tbb::parallel_for_each(intervals.begin(), intervals.end(), [&](int interval) {
-//            DiscreteWorld dworld;
-//            load_grid(dworld, grid_fname(side, interval));
-//        });
-//    }
-//
-//}
+using namespace hls::khalifa::summer;
+
+std::string grid_fname(int side, int interval) {
+    std::string fname = stdx::format(R"(D:\Projects.github\cpp_projects\check_tracks\data\tracksgrid_%d_%d_3months.bin)", side, interval);
+    return fname;
+}
+
+
+void crete_grids() {
+
+    create_grid(5, 0, grid_fname(5, 0));
+
+    std::vector<int> sides{5,10,20,50,100};
+    std::vector<int> intervals{1,5,10,15,30,60};
+
+    for(int side : sides) {
+        tbb::parallel_for_each(intervals.begin(), intervals.end(), [&](int interval) {
+            std::string fname = grid_fname(side, interval);
+            std::cout << fname << std::endl;
+
+            create_grid(side, interval, grid_fname(side, interval));
+        });
+    }
+
+}
+
+void load_grids() {
+
+    {
+        DiscreteWorld dworld;
+        load_grid(dworld, grid_fname(5, 0));
+    }
+
+    std::vector<int> sides{5,10,20,50,100};
+    std::vector<int> intervals{1,5,10,15,30,60};
+
+    for(int side : sides) {
+        tbb::parallel_for_each(intervals.begin(), intervals.end(), [&](int interval) {
+            DiscreteWorld dworld;
+            load_grid(dworld, grid_fname(side, interval));
+        });
+    }
+
+}
 
 //void create_load_grid() {
 //    std::string fname = grid_fname(100, 60);
---
+++ check_tracks/src/other.h
@@ -0,0 +1,8 @@
+//
+// Created by Corrado Mio on 23/09/2020.
+//
+
+#ifndef CHECK_TRACKS_OTHER_H
+#define CHECK_TRACKS_OTHER_H
+
+#endif //CHECK_TRACKS_OTHER_H
