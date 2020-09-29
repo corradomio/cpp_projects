5483179febeae42de3ea5d2f1108db1ea7f78d95
Corrado Mio <corrado.mio@unimi.it>
2020-09-29 07:06:01 +0400

    Update implementation

--- check_gc/main.cpp
+++ check_gc/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <cereal/cereal.hpp>
+#include <gc_cpp.h>
+#include <string.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //std::cout << "Hello, World!" << std::endl;
+
+    char* p = new (GC) char[100];
+    strcpy(p, "Hello World");
+
+    std::cout << p << std::endl;
+
     return 0;
 }
