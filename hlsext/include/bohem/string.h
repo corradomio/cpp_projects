c407d683823dcc42e852468ad1b9db1062439025
Corrado Mio <corrado.mio@unimi.it>
2020-09-30 08:21:18 +0400

    Update implementation

--- hlsext/include/bohem/string.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Corrado Mio on 30/09/2020.
-//
-
-#ifndef HLSEXT_STRING_H
-#define HLSEXT_STRING_H
-
-#endif //HLSEXT_STRING_H
--- hlsext/include/bohem/vector.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Corrado Mio on 30/09/2020.
-//
-
-#ifndef HLSEXT_VECTOR_H
-#define HLSEXT_VECTOR_H
-
-#endif //HLSEXT_VECTOR_H
