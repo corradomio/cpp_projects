19a62b39741edb19837aebb4620680699541d6ca
Corrado Mio <corrado.mio@unimi.it>
2020-10-02 06:24:08 +0400

    Update implementation

---
+++ cmake.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "cmake -G'MSYS Makefiles' -DCMAKE_INSTALL_PREFIX=/usr/local $*"
+/mingw64/bin/cmake -G"MSYS Makefiles" -DCMAKE_INSTALL_PREFIX=/usr/local $*
+#/mingw64/bin/cmake -G"MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=/usr/local $*
