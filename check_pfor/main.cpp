1a59ba38c029adc6e188c686a52bba47d5add869
Corrado Mio <corrado.mio@gmail.com>
2021-05-01 18:09:48 +0400

    Update implementation

--- check_pfor/main.cpp
+++ check_pfor/main.cpp
@@ -4,12 +4,15 @@
 #include <string>
 #include <unordered_set>
 #include <fmt/format.h>
+#include <stdx/hash.h>
+#include <boost/container_hash/hash.hpp>
 
 
 int main(void) {
 
-    int i=10;
-    fmt::print("{}\n", std::hash<int>{}(10));
+    std::hash<float>{}(1.f);
+    std::pair<int, int> p{1,2};
+    fmt::print("{}\n", std::hash<std::pair<int, int>>{}(p));
 
 
     return 0;
