1c4d82184eef0d623991c2fb071be3c740b0c529
Corrado Mio <corrado.mio@gmail.com>
2021-05-22 11:55:42 +0400

    Update implementation

--- check_memory/main.cpp
+++ check_memory/main.cpp
@@ -1,11 +1,55 @@
 #include <iostream>
 
+struct C {
+    static int gid;
+    int id;
+    int* data;
+    C() {
+        id = ++gid;
+        data = new int[100];
+        std::cout << "constructor" << std::endl;
+    }
+    C(const C& c){
+        id = ++gid;
+        data = new int[100];
+        std::cout << "copy constructor" << std::endl;
+    }
+    C(C&& c) {
+        id = ++gid;
+        data = c.data;
+        c.data = nullptr;
+        std::cout << "move constructor" << std::endl;
+    }
+    ~C() {
+        delete[] data;
+        if (data != nullptr)
+            std::cout << "deleted " << id << std::endl;
+        else
+            std::cout << "deleted empty " << id << std::endl;
+    }
+};
+
+int C::gid = 0;
+
+C fun(){
+    C c;
+    for (int i=0; i<100; ++i)
+        c.data[i] = i*i;
+    return c;
+}
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    std::allocator<int> a;
+    C a;
+    C b(a);
+    C c = b;
+    C d = fun();
+    C e(fun());
+    C f(std::move(a));
 
+    std::cout << e.id << std::endl;
 
     return 0;
 }
